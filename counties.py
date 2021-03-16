0989e54f8c0d3e5ef4b631dec752028d938d65da
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-03-16 09:46:08 -0700

    Update counties.py

--- counties.py
+++ counties.py
@@ -1,3 +1,16 @@
 from flask import Flask, request, Markup, render_template, Flash, Markup
 import os
 import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('select.html')
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
