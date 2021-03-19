2ea65dd67690c9467235b9aa432f44a2c9187ef9
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-03-19 09:58:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def render_main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template('select.html', state = get_state_options(counties), fact = get_fact(options, counties))
+    return render_template('select.html', state = get_state_options(counties), fact = get_fact(states, counties))
 
 def get_state_options(counties):
     listOfStates = []
@@ -20,11 +20,11 @@ def get_state_options(counties):
         options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
     return options
 
-def get_fact(options, counties):
+def get_fact(states, counties):
     funFact = "The population of this state (in 2014) was "
     county_pop = 0
     for county in counties:
-        if county["State"] == options:
+        if county["State"] == states:
             county_pop = county_pop + county["Population"]["2014 Population"]
     fun_fact = fun_fact + str(county_pop)
     return fun_fact
